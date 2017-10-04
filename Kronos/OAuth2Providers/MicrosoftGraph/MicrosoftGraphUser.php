697839c8c32798f74bca6f32ef5454b10dbb435f
Eric Durand-Tremblay <etremblay@kronostechnologies.com>
2017-10-04 14:21:11 -0400

    wip

---
+++ Kronos/OAuth2Providers/MicrosoftGraph/MicrosoftGraphUser.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Kronos\OAuth2Providers\Office365;
+
+use League\OAuth2\Client\Provider\ResourceOwnerInterface;
+
+class MicrosoftGraphUser extends \EightyOneSquare\OAuth2\Client\Provider\MicrosoftGraphUser implements ResourceOwnerInterface  {
+}
--- Kronos/OAuth2Providers/Office365/Office365User.php
+++ Kronos/OAuth2Providers/Office365/Office365User.php
@@ -3,8 +3,6 @@
 namespace Kronos\OAuth2Providers\Office365;
 
 use League\OAuth2\Client\Provider\ResourceOwnerInterface;
-use EightyOneSquare\OAuth2\Client\Provider\MicrosoftGraphUser;
 
 class Office365User extends MicrosoftGraphUser implements ResourceOwnerInterface  {
-
 }
