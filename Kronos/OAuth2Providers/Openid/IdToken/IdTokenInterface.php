8632ecbf36277ef2d98e09532bc4b01a0d9d6132
Eric Durand-Tremblay <eric.durand-tremblay@equisoft.com>
2022-05-19 08:57:18 -0400

    QCTECH-119 php 8.1 (#33)
    * php 8.1
    
    * Suppress qodana warning
    
    Co-authored-by: Eric Durand-Tremblay <etremblay@kronostechnologies.com>

--- Kronos/OAuth2Providers/Openid/IdToken/IdTokenInterface.php
+++ Kronos/OAuth2Providers/Openid/IdToken/IdTokenInterface.php
@@ -2,7 +2,7 @@
 
 namespace Kronos\OAuth2Providers\Openid\IdToken;
 
-interface IdTokenInterface
+interface IdTokenInterface extends \JsonSerializable
 {
     /**
      * Constructs an id token.
@@ -25,13 +25,4 @@ interface IdTokenInterface
      * @return mixed
      */
     public function getUserId();
-
-    /**
-     * Specify data which should be serialized to JSON
-     * @link http://php.net/manual/en/jsonserializable.jsonserialize.php
-     * @return mixed data which can be serialized by <b>json_encode</b>,
-     * which is a value of any type other than a resource.
-     * @since 5.4.0
-     */
-    public function jsonSerialize();
 }
